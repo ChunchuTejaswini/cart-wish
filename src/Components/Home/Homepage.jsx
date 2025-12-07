7e5d79dbc49be5ed7e98522fbc8c70ea70949017
ChunchuTejaswini <chunchutejaswini2002@gmail.com>
2025-12-07 17:01:34 +0530

    Update Homepage.jsx

--- src/Components/Home/Homepage.jsx
+++ src/Components/Home/Homepage.jsx
@@ -7,9 +7,9 @@ import Featured from './Featured';
 
 const Homepage = () => {
   return (
-    <div><Herosection title="Buy Iphone 14pro" subtitle="Experience the power of the latest iphone 14 with our most pro camera ever" link="/product/69076439f09bdbeda9527bdf"  image={iphone}/>
+    <div><Herosection title="Buy Iphone 14pro" subtitle="Experience the power of the latest iphone 14 with our most pro camera ever" link="/product/69354b8f1c2fe990e4ff5675"  image={iphone}/>
     <Featured/>
-    <Herosection title="Buy Iphone Ultimate setup" subtitle="You can add studio display and colour-matched magic accessories tp yppur bag after conffigure your mac mini" link="/product/69076439f09bdbeda9527be7"  image={mac}/></div>
+    <Herosection title="Buy Iphone Ultimate setup" subtitle="You can add studio display and colour-matched magic accessories tp yppur bag after conffigure your mac mini" link="/product/69354b8f1c2fe990e4ff5681"  image={mac}/></div>
   )
 }
 
